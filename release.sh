b5dd9c59be0070dab8d29b915656e56d3bbaf018
bartolomeo.sorrentino <bartolomeo.sorrentino@3f03c616-ed4f-0410-a803-1fb4c25bf17e>
2009-06-30 10:36:08 +0000

    upgrade
    git-svn-id: http://swixml2.googlecode.com/svn/trunk/core@297 3f03c616-ed4f-0410-a803-1fb4c25bf17e

--- release.sh
+++
@@ -1,6 +0,0 @@
-
-JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Home
-export JAVA_HOME
-PATH=/opt/subversion/bin:$PATH
-export PATH
-/Applications/apache-maven-2.0.10/bin/mvn -e --batch-mode -Dtag=swixml-2.5.20090630 release:prepare -DreleaseVersion=2.5.20090630 -DdevelopmentVersion=2.5-SNAPSHOT -Dresume=false
