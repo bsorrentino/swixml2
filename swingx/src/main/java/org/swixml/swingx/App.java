4f28e24b362f25c85589c9aed19f9b6f437e001a
bartolomeo.sorrentino <bartolomeo.sorrentino@3f03c616-ed4f-0410-a803-1fb4c25bf17e>
2010-12-04 13:51:04 +0000

    Initial import.
    git-svn-id: http://swixml2.googlecode.com/svn/trunk@785 3f03c616-ed4f-0410-a803-1fb4c25bf17e

---
+++ swingx/src/main/java/org/swixml/swingx/App.java
@@ -0,0 +1,13 @@
+package org.swixml.swingx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
