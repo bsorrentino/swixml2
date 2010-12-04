f493edc381eef50f9a8c5d5772d9844c704b2ea5
bartolomeo.sorrentino <bartolomeo.sorrentino@3f03c616-ed4f-0410-a803-1fb4c25bf17e>
2010-12-04 09:08:47 +0000

    test 4 issue60
    git-svn-id: http://swixml2.googlecode.com/svn/trunk@778 3f03c616-ed4f-0410-a803-1fb4c25bf17e

---
+++ examples/showcase/src/main/java/issue60/Issue60Frame.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package issue60;
+
+import javax.swing.JFrame;
+import javax.swing.JSplitPane;
+
+/**
+ *
+ * @author bsorrentino
+ */
+public class Issue60Frame extends JFrame{
+
+    public JSplitPane split;
+
+
+    
+}
